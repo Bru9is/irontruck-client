8f4502b85669307adb3b8a8461fc13ecd3ac8ce6
kseniabusquet <ksenia.busquet@gmail.com>
2022-08-20 16:01:42 -0300

    front signup and login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import '../assets/styles/index.css'
 
 function Home() {
   return (
     <div className="text-center">
       <Navbar />
-      <img
-        src="https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/84/original/logo-ironhack-blue.png"
-        alt="ironhack logo"
-      />
-      <h1>React IronPlate</h1>
-      <p>This is the homepage</p>
-      <div className="d-flex flex-column align-items-center">
-        <Link className="btn btn-lg btn-primary" to="/auth/signup">
-          Signup here!
-        </Link>
+      <h1>IronTruck</h1>
+      <div className="d-flex flex-column align-items-center flex-container">
+        <div>
+          <div className = 'signup-container'>
+            Signup as a company
+          </div>
+          <Link className="btn btn-lg btn-primary" to="/auth/signup?type=company">
+            Signup
+          </Link>
+        </div>
+
+        <div>
+          <div className = 'signup-container'>
+            Signup as a user
+          </div>
+          <Link className="btn btn-lg btn-primary" to="/auth/signup?type=user">
+            Signup
+          </Link>
+        </div>
       </div>
+
+      <Link to="/auth/login">
+          Already have an account? Click here to login.
+      </Link>
     </div>
   );
 }
--- src/pages/auth/Signup.js
+++ src/pages/auth/Signup.js
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import apiService from "../../services/api.service";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import apiService from "../../services/api.service"; 
 
 function Signup(props) {
+
+  const [searchParams] = useSearchParams()
+  console.log(searchParams.get('type'))
+
   const [state, setState] = useState({ 
-                              name: "", 
-                              password: "", 
-                              email: "", 
-                              phone: "", 
-                              address: {street:"", number:"", city:"", state:"", zip:""}, 
-                              role: "", 
-                              document: "" });
+    name: "", 
+    password: "", 
+    email: "", 
+    phone: "", 
+    street:"", 
+    number:"", 
+    city:"", 
+    state:"", 
+    zip:"", 
+    role: searchParams.get('type'), 
+    document: "" });
+
   const [errors, setErrors] = useState({
     name: null,
     email: null,
     password: null,
     role: null,
     phone: null,
-    address: {street:null, number:null, city:null, state:null, zip:null},
+    street:null, 
+    number:null, 
+    city:null, 
+    state:null, 
+    zip:null,
     document: null,
   });
 
@@ -34,8 +47,23 @@ function Signup(props) {
     event.preventDefault();
 
     try {
-      await apiService.signUp(state);
-      setErrors({ name: "", password: "", email: "", phone: "", address: {street:""}, role: "", document: "" });
+      const newUser = {
+        name: state.name,
+        email: state.email,
+        password: state.password,
+        phone: state.phone,
+        role: state.role,
+        address: {
+          street: state.street,
+          number: state.number,
+          city: state.city,
+          state: state.state,
+          zip: state.zip
+        },
+        document: state.document
+      }
+      await apiService.signUp(newUser);
+      setErrors({ name: "", password: "", email: "", phone: "", street:"", number:"", city:"", state:"", zip:"", document: "" });
       navigate("/auth/login");
     } catch (err) {
       console.error(err);
@@ -45,7 +73,7 @@ function Signup(props) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h1>Signup!</h1>
+      <h1>{state.role} signup</h1>
 
       <div>
         <label htmlFor="signupFormName">Name</label>
@@ -84,34 +112,92 @@ function Signup(props) {
       </div>
 
       <div>
-        <label htmlFor="signupFormRole">Role</label>
-        <select id="signupFormRole"
-          value={state.role}
-          error={errors.role}
-          onChange={handleChange}>
-        <option>User</option>
-        <option>Company</option>
-        </select>
+        <label htmlFor="signupFormPhone">Phone</label>
+        <input
+          type="text"
+          name="phone"
+          id="signupFormPhone"
+          value={state.phone}
+          error={errors.phone}
+          onChange={handleChange}
+        />
       </div>
 
       <div>
         <label htmlFor="signupFormStreet">Street</label>
         <input
           type="text"
-          name="name"
+          name="street"
           id="signupFormStreet"
-          value={state.address.street}
-          error={errors.address.street}
+          value={state.street}
+          error={errors.street}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="signupFormNumber">Number</label>
+        <input
+          type="text"
+          name="number"
+          id="signupFormNumber"
+          value={state.number}
+          error={errors.number}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="signupFormCity">City</label>
+        <input
+          type="text"
+          name="city"
+          id="signupFormCity"
+          value={state.city}
+          error={errors.city}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="signupFormCity">State</label>
+        <input
+          type="text"
+          name="state"
+          id="signupFormState"
+          value={state.state}
+          error={errors.state}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="signupFormCity">Zipcode</label>
+        <input
+          type="text"
+          name="zip"
+          id="signupFormZip"
+          /*pattern="	^\d{5}-\d{3}$"*/
+          value={state.zip}
+          error={errors.zip}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="signupFormDocument">{state.role==='user'? "CPF" : "CNPJ"}</label>
+        <input
+          type="text"
+          name="document"
+          id="signupFormDocument"
+          value={state.document}
+          error={errors.document}
           onChange={handleChange}
         />
       </div>
 
       <div>
         <button type="submit">Signup!</button>
-
-        <Link to="/auth/login">
-          Already have an account? Click here to login.
-        </Link>
       </div>
     </form>
   );
